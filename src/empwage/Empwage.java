5e82b5ef9ad6a8ae2b9fb6637b818f8730519ce1
Shahid-sk <mail2sk.shahid@gmail.com>
2021-07-01 20:39:46 +0530

    employee check

--- src/empwage/Empwage.java
+++ src/empwage/Empwage.java
@@ -2,7 +2,13 @@ package empwage;
 
 public class Empwage {
 	public static void main(String[] args) {
+		int IS_EMP_PRESENT = 1;
 		System.out.println("Welcome to Employee wage simulation program");
+		double empcheck=Math.floor(Math.random()*10)%2;
+		if(empcheck == IS_EMP_PRESENT)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 
 }
